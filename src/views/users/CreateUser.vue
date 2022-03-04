25b7a40af64ea7d061083f7f63a082506e6f15c1
RandalphvanVelden <45844254+RandalphvanVelden@users.noreply.github.com>
2022-03-04 13:33:42 +0100

    working on login

--- src/views/users/CreateUser.vue
+++ src/views/users/CreateUser.vue
@@ -1,3 +1,74 @@
 <template>
-    <h1>Hier kan je een user maken</h1>
+    <form @submit.prevent="userCreate()">
+        <div class:="form-control" >
+            <label for="name" >Name</label>
+            <input type="text" id="name" v-model.trim="name">  
+        </div>
+         <div class:="form-control" >
+            <label for="email">email</label>
+            <input type="email" id="email" v-model.trim="email" >
+        </div>
+         <div class:="form-control">
+            <label for="password">Password</label>
+            <input type="password"  id="password" v-model.trim="password" >
+        </div>         
+
+        <button>Register</button>
+    </form>
 </template>
+
+<script>
+import {ref} from 'vue'
+import { useMutation } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
+
+export default {
+setup(){
+    const name = ref('')
+    const email = ref('')
+    const password = ref('')
+
+    const {mutate: createUser, onDone} = useMutation(gql`
+    mutation createUser($name: String!, $email: String!, $password: String!) {
+  createUser(name: $name, email: $email, password: $password){
+    id
+    name
+   email
+  }
+}
+    `, ()=>({
+        variables:{
+        name: name.value,
+        email: email.value,
+        password: password.value
+        },
+   
+    }))
+    
+    onDone(() =>{
+    
+    })
+    return{
+        name,
+        email,
+        password,
+        createUser,
+        
+    }
+},
+methods:{
+
+returnToHome(){
+    this.$router.replace('/')
+},
+    userCreate(){
+        this.returnToHome()
+        this.createUser()
+    }
+}
+};
+</script>
+<style scoped>
+
+</style>
--- src/views/users/TheLogin.vue
+++ src/views/users/TheLogin.vue
@@ -1,11 +1,73 @@
 <template>
-    <h1>Hier kan je inloggen</h1>
+    <form @submit.prevent="login()">
+         <div class:="form-control" >
+            <label for="email">email</label>
+            <input type="email" id="email" v-model.trim="email" >
+        </div>
+         <div class:="form-control">
+            <label for="password">Password</label>
+            <input type="password"  id="password" v-model.trim="password" >
+        </div>         
+    <div>
+        <p>{{token}}</p>
+        </div>
+        <button>Register</button>
+        <div><p>{{token}}</p></div>
+    </form>
 </template>
 
 <script>
+import {ref} from 'vue'
+import { useMutation, useResult} from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
+
 export default {
-    setup() {
-      
-    },
+setup(){
+    const email = ref('')
+    const password = ref('')
+    const username = ''
+
+    const {mutate: login, result, onDone} = useMutation(gql`
+   mutation login($username: String!, $password: String!){
+  login(input:{
+    username: $username,
+    password: $password
+  }){
+    user{
+      name
+    }
+    access_token
 }
+}
+    `, ()=>({
+        variables:{
+      
+        username: email.value,
+        password: password.value
+        },
+   
+    }))
+    
+    onDone(() =>{
+    
+    })
+    const token = useResult(result, null, data => data.login)
+    return{
+        token,
+        email,
+        username,
+        password,
+        login,
+        
+    }
+},
+methods:{
+
+
+}
+};
 </script>
+<style scoped>
+
+</style>
